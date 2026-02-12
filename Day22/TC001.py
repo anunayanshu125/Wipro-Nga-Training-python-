8368a456d5c8b6fbf74e2ef24347ea449f4e7c1f
anunayanshu125 <118728462+anunayanshu125@users.noreply.github.com>
2026-02-12 23:46:31 +0530

    codes

---
+++ Day22/TC001.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+host = "localhost"
+user = "root"
+password = "Anshu@123"
+database = "feb_2026"
+conn = mysql.connector.connect(host=host, user=user, password=password, database=database)
+cursor = conn.cursor()
+print("connected to the database successfully")
+query = "SELECT * FROM employee"
+cursor.execute(query)
+result = cursor.fetchall()
+for row in result:
+    print(row)
+
