054f11f583070708b609c4201da529fb2219d351
anunayanshu125 <118728462+anunayanshu125@users.noreply.github.com>
2026-02-13 22:13:55 +0530

    Assignments Day 22

---
+++ Class assignments Day23(13 feb 2026)/q2.py
@@ -0,0 +1,33 @@
+import math
+import time
+from multiprocessing import Pool, cpu_count
+
+numbers = [5000, 6000, 5500, 4500, 7000]
+
+
+def compute_factorial(n):
+    return math.factorial(n)
+
+
+if __name__ == "__main__":
+
+
+
+    starttime = time.perf_counter()
+
+    seq_results = []
+    for num in numbers:
+        result = compute_factorial(num)
+        seq_results.append(result)
+
+    sequential_time = time.perf_counter() - starttime
+    print("Sequential Time:", sequential_time)
+
+
+    starttime2 = time.perf_counter()
+
+    with Pool(cpu_count()) as pool:
+        parallel_result = pool.map(compute_factorial, numbers)
+
+    parallel_time = time.perf_counter() - starttime2
+    print("Multiprocessing Time:", parallel_time)
